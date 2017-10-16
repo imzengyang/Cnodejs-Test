575c45976fac746321b415e56fe298b2aab4345c
Zack <zack@leanpro.cn>
2017-10-16 22:33:53 +0800

    assert elements for option

--- test.js
+++ test.js
@@ -1,2 +1,34 @@
-let data = require('./testdata/register.json')
-console.log(data.emailerrData.userName);
+// let data = require('./testdata/register.json')
+// console.log(data.emailerrData.userName);
+
+
+require('chromedriver')
+let { Builder } = require('selenium-webdriver')
+
+let uiaction = require("./common/ui/uiaction")
+let assert = require("assert")
+
+driver = new Builder().forBrowser('chrome').build();
+driver.manage().window().maximize();
+
+let testGetText = async function(driver,username,password) {
+    await uiaction.userLogin(driver,username,password);
+    await driver.get("http://118.31.19.120:3000/topic/create");
+    let alltext = await driver.findElements({css:"#tab-value > option"})
+    let arr = [];
+    for(let option of alltext){
+        let optionName = await option.getText();
+        console.log(optionName)
+        arr.push(optionName);
+    }
+    console.log(alltext.length)
+
+    let actual = ["请选择","分享","问答","招聘"]
+    assert.deepEqual(actual,arr)
+}
+
+testGetText(driver,"imzack","123456");
+
+
+
+
